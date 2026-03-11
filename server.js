a044c08abae2bbe2b394d6a290ccf2ab6a343713
Yuval Greenfield <ubershmekel@gmail.com>
2026-03-10 22:59:30 -0700

    hello2

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const http = require('http');
+const http = require("http");
 
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<html><body>hello</body></html>');
-}).listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<html><body>hello2</body></html>");
+  })
+  .listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
